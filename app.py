ffd5ff9c0bff16d7eadf30abdd81c77488cfd3fc
C.Logesh Perumal <125385633+C-Logesh-Perumal-29@users.noreply.github.com>
2024-03-10 17:45:54 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# Importing the libraries :
 import cv2
 import numpy as np
 from PIL import Image
@@ -7,8 +6,8 @@ import base64
 from io import BytesIO
 
 # Icon & title of the page :
-img = Image.open("13.jpg")
-st.set_page_config(page_title="Certificate Generation",page_icon=img,layout="wide")
+img = Image.open("ico.png")
+st.set_page_config(page_title="Certificate Generation", page_icon=img, layout="wide")
 
 # Hide Menu_Bar & Footer :
 hide_menu_style = """
@@ -17,138 +16,130 @@ hide_menu_style = """
     footer {visibility : hidden;}
     </style>
 """
-st.markdown(hide_menu_style , unsafe_allow_html=True)
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 # Set the background image :
-
 Background_image = """
-
 <style>
 [data-testid="stAppViewContainer"] > .main
 {
-background-image: url("https://img.freepik.com/premium-vector/abstract-blur-bokeh-lens-flare-vintage-blue-purple_147586-112.jpg?size=626&ext=jpg&ga=GA1.2.2087154549.1663432512&semt=ais");
-
-background-size : 100%
+background-image: url("https://img.freepik.com/free-photo/artistic-blurry-colorful-wallpaper-background_58702-8616.jpg?t=st=1710070184~exp=1710073784~hmac=1b64d09e720f0c0b866bae342fa5a3c594febde4a3b93ba824dadd650d02d22d&w=996");
+background-size : 100%;
 background-position : top left;
-
 background-position: center;
 background-size: cover;
 background-repeat : repeat;
 background-repeat: round;
-
-
 background-attachment : local;
-
-background-image: url("https://img.freepik.com/premium-vector/abstract-blur-bokeh-lens-flare-vintage-blue-purple_147586-112.jpg?size=626&ext=jpg&ga=GA1.2.2087154549.1663432512&semt=ais");
+background-image: url("https://img.freepik.com/free-photo/artistic-blurry-colorful-wallpaper-background_58702-8616.jpg?t=st=1710070184~exp=1710073784~hmac=1b64d09e720f0c0b866bae342fa5a3c594febde4a3b93ba824dadd650d02d22d&w=996");
 background-position: right bottom;
 background-repeat: no-repeat;
 }  
-
 [data-testid="stHeader"]
 {
 background-color : rgba(0,0,0,0);
 }
-
 </style>                                
 """
+st.markdown(Background_image, unsafe_allow_html=True)
 
-st.markdown(Background_image,unsafe_allow_html=True)
-
-col1,col2 = st.columns([1,5])
-
-with col1:
-    img = Image.open("13.jpg")
-    st.image(img,width=150)
+col1, col2, col3 = st.columns([4, 2, 4])
 
 with col2:
-    
-    h = """
-        <html>
-            <head>
-                <title></title>
-            </head>
-            <body>
-                <h1 style = "align:right;font-family:Engravers MT;color:#191970"> Certificate Generation </h1>
-            </body>
-        </html>
-    """
-    st.markdown(h,unsafe_allow_html=True)
+    img = Image.open("Icon.png")
+    st.image(img, width=100, use_column_width=True, output_format='auto')
+
+h = """
+    <html>
+        <head>
+            <style>
+                .title {
+                    text-align: center;
+                    font-family: 'Engravers MT';
+                    color: white;
+                }
+            </style>
+        </head>
+        <body>
+            <div style="display: flex; align-items: center; justify-content: center;">
+                <h1 class="title">Certificate Generation</h1>
+            </div>
+        </body>
+    </html>
+"""
+st.markdown(h, unsafe_allow_html=True)
 
 st.balloons()
 
 Name_List_Data = []
 
 def Name_List():
-    
-        file_uploader = st.file_uploader("Choose the file",type = ['txt'])
-        if file_uploader is not None:
-            file_path = file_uploader.name 
-            st.success("File Uploaded Successfully...")
-      
-        file = open(file_path,"r")    
+    file_uploader = st.file_uploader("Choose the file", type=['txt'])
+    if file_uploader is not None:
+        file_path = file_uploader.name
+        st.success("File Uploaded Successfully...")
+
+        file = open(file_path, "r")    
         for names in file:
             Name_List_Data.append(names.strip())
             
         st.write(Name_List_Data)
 
+    return Name_List_Data
 
 def generate_certificate():
-
-    coordinate_1 = st.text_input("Enter Coordinate 1 : ",placeholder="X Coordinate")
+    coordinate_1 = st.text_input("Enter Coordinate 1 : ", placeholder="X Coordinate")
+    coordinate_2 = st.text_input("Enter Coordinate 2 : ", placeholder="Y Coordinate")
     
-    if coordinate_1:
+    # Use select boxes for RGB color code
+    rgb_color = st.text_input("RGB Color Code : ", placeholder="RGB Color (e.g., 255,255,255)")
+
+    if rgb_color:
+        rgb_values = rgb_color.split(',')
+        if len(rgb_values) == 3:
+            try:
+                Red = int(rgb_values[0])
+                Green = int(rgb_values[1])
+                Blue = int(rgb_values[2])
+            except ValueError:
+                st.error("Invalid RGB color values. Please enter integers separated by commas.")
+                return
+        else:
+            st.error("Invalid RGB color format. Please enter three integer values separated by commas.")
+            return
+
+    certificate_uploader = st.file_uploader("Upload the Certificate : ", type=["jpg", "jpeg", "png"])
     
-        coordinate_2 = st.text_input("Enter Coordinate 2 : ",placeholder="Y Coordinate")
+    if certificate_uploader is not None:
+        file_path = certificate_uploader.name
         
-    if coordinate_2:
-            
-        RGB = st.text_input("RGB Color Code : ",placeholder="RGB Color")
-        l = RGB.split(',')
-        Red = int(l[0])
-        Green = int(l[1])
-        Blue = int(l[2])
-
-        Certificate_Uploader = st.file_uploader("Upload the Certificate : ",type = ["jpg","jpeg","png"])
-        st.image(Certificate_Uploader,caption="Your Certificate Template")
-
+        # Display the uploaded certificate image
+        st.image(certificate_uploader, caption="Your Certificate Template")
         
-    for index,name in enumerate(Name_List_Data):
-         
-        if Certificate_Uploader is not None:
-            
-            file_path = Certificate_Uploader.name
-                
-            image = Image.open(Certificate_Uploader)
-            
+        for name in Name_List_Data:
+            image = Image.open(certificate_uploader)
             image = np.array(image)
-    
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-              
-            cv2.putText(image,name,(int(coordinate_1),int(coordinate_2)),cv2.FONT_HERSHEY_SIMPLEX,2,((int(Blue),int(Green),int(Red))),6,cv2.LINE_AA)
-            
-            cv2.imwrite(f'{name}.jpg',image)
-            
+
+            cv2.putText(image, name, (int(coordinate_1), int(coordinate_2)), cv2.FONT_HERSHEY_SIMPLEX, 2,
+                        ((int(Blue), int(Green), int(Red))), 6, cv2.LINE_AA)
+
+            cv2.imwrite(f'{name}.jpg', image)
+
             st.write(f'Processing  {name}\'s Certificate')
-            
-            def get_image_download_link(img,filename,text):
+
+            def get_image_download_link(img, filename, text):
                 buffered = BytesIO()
                 img.save(buffered, format="JPEG")
                 img_str = base64.b64encode(buffered.getvalue()).decode()
-                href =  f'<a href="data:file/jpg;base64,{img_str}" download="{filename}">{text}</a>'
+                href = f'<a href="data:file/jpg;base64,{img_str}" download="{filename}">{text}</a>'
                 return href
-            
+
             image = np.array(image)
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             result = Image.fromarray(image)
-            st.markdown(get_image_download_link(result,file_path,'Download Image'),unsafe_allow_html=True)
-            
-        else:
-            print('File not available')  
+            st.markdown(get_image_download_link(result, file_path, 'Download Image'), unsafe_allow_html=True)
 
-    st.info("\n !!!!!!!!!!!!!!!! Certificates Generated Successfully !!!!!!!!!!!!!!!!")
-    st.balloons() 
-    
+# Run functions
 Name_List()
-
 generate_certificate()
-
